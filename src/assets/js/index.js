27e04fcd110f593cc86e1c470d0b41bd9ee1d434
PKLJack <59538991+PKLJack@users.noreply.github.com>
2025-05-10 18:10:56 +0100

    Add basic portfolio modal template

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -81,3 +81,144 @@ contactFormEl
     // - Take 200 or OK and show user success
     //
   });
+
+/* ======================================== */
+/* Portfolio Modal                          */
+/* ======================================== */
+// TODO: Put to portfolio section
+//
+
+const portfolioModalArr = [
+  {
+    id: "proj-gazetteer-modal",
+    title: "Gazetteer",
+    imgSrc: "assets/img/portfolio/gazetteer_demo_1-600x800.png",
+    description:
+      "A web application where users can select different countries and view their demographic, climate, geographical and other data.",
+    information: {
+      category: "Full stack web application",
+      date: "October, 2024",
+    },
+    links: {
+      live: "#",
+      // github: "#",
+    },
+    stack: [
+      // prettier-ignore
+      "JavaScript",
+      ["Leaflet", "jQuery"],
+      "CSS",
+      ["Bootstrap 5"],
+      "PHP",
+    ],
+  },
+  {
+    id: "proj-companydirectory-modal",
+    title: "Company Directory",
+    imgSrc: "assets/img/portfolio/companydirectory_demo_1-600x800.png",
+    description:
+      "A desktop website that can also run on a mobile that allows for the maintenance of a company personnel database to see who’s who, which department they are in and where they are.",
+    information: {
+      category: "Full stack web application",
+      date: "January, 2025",
+    },
+    links: {
+      live: "#",
+      // github: "#",
+    },
+    stack: [
+      // prettier-ignore
+      "JavaScript",
+      ["jQuery"],
+      "CSS",
+      ["Bootstrap 5"],
+      "PHP",
+      "MariaDB",
+    ],
+  },
+];
+
+function generateModal(obj) {
+  // TODO:
+
+  /** @type{DocumentFragment} */
+  const templateContent = document.querySelector(
+    "#proj-model-template",
+  ).content;
+
+  // const frag = document.createDocumentFragment()
+
+  /** @type{DocumentFragment} */
+  const tmpEl = templateContent.cloneNode(true);
+
+  // Update id
+  tmpEl.querySelector(".modal").id = obj.id;
+
+  // Update aria-labelled-by
+  tmpEl
+    .querySelector(".modal")
+    .setAttribute("aria-labelledby", `${obj.id}-label`);
+
+  // Update h1, h1 id
+  tmpEl.querySelector(".modal-title").id = `${obj.id}-label`;
+  tmpEl.querySelector(".modal-title").textContent = obj.title;
+
+  // Update image
+  tmpEl.querySelector("img").src = obj.imgSrc;
+
+  // Update description
+  tmpEl.querySelector(".project-description").textContent = obj.description;
+
+  // Update information ul
+  tmpEl.querySelector(".project-category").textContent =
+    obj.information.category;
+  tmpEl.querySelector(".project-date").textContent = obj.information.date;
+
+  // Add website if exists
+  if (obj.links.live) {
+    const liveLiEl = document.createElement("li");
+    liveLiEl.innerHTML = `<a href="${obj.links.live}">Visit Website</a>`;
+    tmpEl.querySelector(".project-information").append(liveLiEl);
+  }
+
+  // Add Github Repo if exists
+  if (obj.links.github) {
+    const githubLiEl = document.createElement("li");
+    githubLiEl.innerHTML = `<a href="${obj.links.github}">Visit Website</a>`;
+    tmpEl.querySelector(".project-information").append(githubLiEl);
+  }
+
+  // Generate stack ul
+  const createLiEl = (textContent) => {
+    const liEl = document.createElement("li");
+    liEl.textContent = textContent;
+    return liEl;
+  };
+
+  const ulChildren = obj.stack.map((item) => {
+    if (typeof item === "string") {
+      return createLiEl(item);
+    }
+
+    if (Array.isArray(item)) {
+      const ulElement = document.createElement("ul");
+      for (x of item) {
+        ulElement.append(createLiEl(x));
+      }
+      return ulElement;
+    }
+
+    throw new Error("Unknown type");
+  });
+
+  tmpEl.querySelector(".project-stack").replaceChildren(...ulChildren);
+
+  return tmpEl;
+}
+
+document.body.append(generateModal(portfolioModalArr[0]));
+document.body.append(generateModal(portfolioModalArr[1]));
+
+function generateAllModals(arr) {
+  //
+}
